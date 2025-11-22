767a75b06eafa21fac8826f10ee6078af60c5d2f
Felipe Neves <fneves@1500th.com>
2025-11-22 17:36:33 -0300

    Envia cardapio

--- src/banco/supabase.ts
+++ src/banco/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://eoiqxmwqjttxtsxxmgms.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvaXF4bXdxanR0eHRzeHhtZ21zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM3NjA5MjMsImV4cCI6MjA3OTMzNjkyM30.igJDHT37NfneynJ45t1IBdz3gqjnXrqUrT8KmsntEr8'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
